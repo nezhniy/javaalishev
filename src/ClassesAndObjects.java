bbb3df7517e46ab533e4829d1348cd1f61c64582
nezhniy <nezhniystrastniy@yandex.ru>
2022-03-23 11:53:11 +0500

    Initial commit

---
+++ src/ClassesAndObjects.java
@@ -0,0 +1,26 @@
+public class ClassesAndObjects {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.name = "Roma";
+        person1.age = 50;
+        person1.sayHello();
+
+        Person person2 = new Person();
+        person2.name = "Vova";
+        person2.age = 20;
+        person2.speak();
+    }
+}
+
+class Person{
+    String name;
+    int age;
+
+    void speak(){
+        for (int i = 0; i < 3; i++)
+            System.out.println("My name is "+name+", im "+age+".");
+    }
+    void sayHello(){
+        System.out.println("Hi!");
+    }
+}
